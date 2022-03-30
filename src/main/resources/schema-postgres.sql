1d739612797e7c3af36f02bd4e530ba913b85b23
gonzalo azcarater <gonzazca108@gmail.com>
2022-03-30 21:29:45 +0200

    Tabla clientes actualizada.

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,6 +1,10 @@
 DROP TABLE IF EXISTS clients;
 CREATE TABLE clients (
 	id SERIAL PRIMARY KEY,
-	first_name VARCHAR(255) NOT NULL,
-	last_name VARCHAR(255)
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	phone_number VARCHAR(12) ,
+	medical_observations VARCHAR(500),
+	observations VARCHAR(500)
 );
