9ec474dbe18dc54649acf833003d4e91b393c26f
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2022-03-30 22:09:05 +0200

    modified example data to match model

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,5 +1,5 @@
-INSERT INTO clients(first_name, last_name) VALUES ('Andres', 'Cienfuegos');
-INSERT INTO clients(first_name, last_name) VALUES ('Samu', 'García');
-INSERT INTO clients(first_name, last_name) VALUES ('Claudia', 'Fernández');
-INSERT INTO clients(first_name, last_name) VALUES ('Gonzalo', 'Azcarate');
+INSERT INTO clients(first_name, last_name, email) VALUES ('Andres', 'Cienfuegos', 'andres.cienfuegos@gmail.com');
+INSERT INTO clients(first_name, last_name, phone) VALUES ('Samu', 'García', '+56987654321');
+INSERT INTO clients(first_name, last_name, observations) VALUES ('Claudia', 'Fernández', 'la mejor');
+INSERT INTO clients(first_name, last_name, medical_observations) VALUES ('Gonzalo', 'Azcarate', 'no sabe poner comas xd');
 INSERT INTO clients(first_name, last_name) VALUES ('Ana', 'Méndez');
