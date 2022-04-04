e8f9a0492070574ba7e50d6ea79a52ede3975e66
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2022-04-04 02:30:05 +0200

    bugfix: missing not null field in data examples

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,8 +1,8 @@
 INSERT INTO clients(first_name, last_name, email, dni) VALUES ('Andres', 'Cienfuegos', 'andres.cienfuegos@gmail.com', '12345678A');
-INSERT INTO clients(first_name, last_name, phone, dni) VALUES ('Samu', 'García', '+56987654321', '12345678A');
-INSERT INTO clients(first_name, last_name, observations, dni) VALUES ('Claudia', 'Fernández', 'la mejor', '12345678A');
-INSERT INTO clients(first_name, last_name, medical_observations, dni) VALUES ('Gonzalo', 'Azcarate', 'no sabe poner comas xd', '12345678A');
-INSERT INTO clients(first_name, last_name, dni) VALUES ('Ana', 'Méndez', '12345678A');
+INSERT INTO clients(first_name, last_name, phone, dni, email) VALUES ('Samu', 'García', '+56987654321', '12345678A', 'samu@mail.com');
+INSERT INTO clients(first_name, last_name, observations, dni, email) VALUES ('Claudia', 'Fernández', 'la mejor', '12345678A', 'claudia@mail.com');
+INSERT INTO clients(first_name, last_name, medical_observations, dni, email) VALUES ('Gonzalo', 'Azcarate', 'No le gusta la asignatura de ISST', '12345678A', 'gonzalo@mail.com');
+INSERT INTO clients(first_name, last_name, dni, email) VALUES ('Ana', 'Méndez', '12345678A', 'ana@mail.com');
 
 INSERT INTO treatments(name, price, duration) VALUES ('Tratamiento 1', '100', '30');
 INSERT INTO treatments(name, price, duration) VALUES ('Tratamiento 2', '200', '60');
