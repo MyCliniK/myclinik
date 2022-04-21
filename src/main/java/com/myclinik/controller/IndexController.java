1eb925bc3b2e855d10e0ec5433185e8d97264887
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2022-04-22 01:14:50 +0200

    feature: calendar read only

--- src/main/java/com/myclinik/controller/IndexController.java
+++ src/main/java/com/myclinik/controller/IndexController.java
@@ -1,12 +1,24 @@
 package com.myclinik.controller;
 
+import com.myclinik.model.Appointment;
+import com.myclinik.service.IAppointmentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
+	@Autowired
+	private IAppointmentService appointmentService;
+
 	@GetMapping("/home")
-	public String goToHomePage() {
-        return ("home");
+	public String goToHomePage(Model model) {
+		var appointments = (List<Appointment>) appointmentService.findAll();
+		model.addAttribute("appointments", appointments);
+        return ("calendar");
     }
 }
