e64e321deb611e2f0253cbbad3a416401e9dca4a
claudiagfg <claudiagaroe.fernandez@gmail.com>
2022-04-27 20:03:51 +0200

    added date filter

--- src/main/java/com/myclinik/controller/StatisticsController.java
+++ src/main/java/com/myclinik/controller/StatisticsController.java
@@ -9,6 +9,7 @@ import com.myclinik.service.IClientService;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.bytebuddy.asm.Advice.Local;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,7 +49,8 @@ public class StatisticsController {
 	}
 	@RequestMapping(value = "/statistics", params= "clientId")
 	public String filterByClient(Model model, @RequestParam("clientId")  Long clientId){
-        var clients = (List<Client>) clientService.findAll();
+        System.out.println("Has entrado al request de clientid");
+		var clients = (List<Client>) clientService.findAll();
 		model.addAttribute("clients", clients);
         model.addAttribute("clientId", clientId);
         return "statistics";
@@ -69,14 +74,23 @@ public class StatisticsController {
 	public String filterByDone(Model model, @RequestParam("appointmentDone")  Boolean appointmentDone){
 		var appointments = (List<Appointment>) appointmentService.findAll();
 		model.addAttribute("appointments", appointments);
-		model.addAttribute("appointmentPaid", appointmentDone);
+		model.addAttribute("appointmentDone", appointmentDone);
+		return "statistics";
+	}
+	@RequestMapping(value = "/statistics", params= "appointmentDate")
+	public String filterByDate(Model model, @RequestParam ("appointmentDate") LocalDateTime appointmentDate){
+		System.out.println("Has entrado al request de date");
+		var appointments = (List<Appointment>) appointmentService.findAll();
+		model.addAttribute("appointments", appointments);
+		model.addAttribute("appointmentDate", appointmentDate);
 		return "statistics";
 	}
 
 	
     @PostMapping("/statistics")
-    public String filterData(Model model, @RequestParam(name = "client", required = false) Client client, @RequestParam(name= "treatment", required = false) Treatment treatment, @RequestParam(name="paidAppointment", required = false) Boolean paidAppointment, @RequestParam(name="doneAppointment", required = false) Boolean doneAppointment){
-        var treatments = treatmentService.findAll();
+    public String filterData(Model model, @RequestParam(name = "client", required = false) Client client, @RequestParam(name= "treatment", required = false) Treatment treatment, @RequestParam(name="paidAppointment", required = false) Boolean paidAppointment, @RequestParam(name="doneAppointment", required = false) Boolean doneAppointment, @RequestParam(name="initialDate", required = false) String initialDate, @RequestParam(name="endDate", required = false) String endDate  ){
+        System.out.println("has entrado al Post");
+		var treatments = treatmentService.findAll();
         var clients = (List<Client>) clientService.findAll();
         var allAppointments = (List<Appointment>) appointmentService.findAll();
 		var appointments = allAppointments;
@@ -84,6 +98,11 @@ public class StatisticsController {
 		if (treatment !=null) appointments = appointments.stream().filter(appointment -> appointment.getTreatment().getId() == treatment.getId()).collect(java.util.stream.Collectors.toList());
 		if (paidAppointment != null) appointments = appointments.stream().filter(appointment -> appointment.getPaid() == paidAppointment).collect(java.util.stream.Collectors.toList());
         if (doneAppointment != null) appointments = appointments.stream().filter(appointment -> appointment.getDone() == doneAppointment).collect(java.util.stream.Collectors.toList());
+		System.out.println("inicio: " + initialDate + " end: " + endDate);
+		LocalDateTime inicioDate = LocalDateTime.parse(initialDate);
+		LocalDateTime finalDate = LocalDateTime.parse(endDate);
+		if (initialDate != null) appointments = appointments.stream().filter(appointment -> !appointment.getAppointmentDate().isBefore(inicioDate)).collect(java.util.stream.Collectors.toList());
+		if (endDate != null) appointments = appointments.stream().filter(appointment -> !appointment.getAppointmentDate().isAfter(finalDate)).collect(java.util.stream.Collectors.toList());
 		model.addAttribute("treatments",treatments);
         model.addAttribute("clients", clients);
         model.addAttribute("appointments", appointments);
@@ -91,10 +110,11 @@ public class StatisticsController {
         if (treatment != null) model.addAttribute("treatmentId", treatment.getId());
 		model.addAttribute("paidAppointment", paidAppointment);
 		model.addAttribute("doneAppointment", doneAppointment);
+		if (initialDate != null) model.addAttribute("initialDate", initialDate);
+		if (endDate != null) model.addAttribute("endDate", endDate);
         return "statistics";
     }
 
 	
 
 }
-//).stream().filter(appointment -> appointment.getTreatment().getId() == treatmentId).collect(java.util.stream.Collectors.toList());
